c6ca366b61f52ddb2a954913eb8cca7e294857bb
TheCryptedFantom <lifeforgames09@gmail.com>
2026-09-17 10:22:14 -0600

    Added Comments Explaining

--- LastNameHW5/Scripts/scripts.js
+++ LastNameHW5/Scripts/scripts.js
@@ -2,9 +2,9 @@ var firstName = "Trenton";
 var favoriteGame = "Minecraft";
 var otherFavoriteGame = "Ark: Survival Evolved";
 var age = 17;
-var gamesOwnedonSteam = 61;
-var gamesOwnedonPlaystation = 51;
-var totalGamesOwned = gamesOwnedonSteam + gamesOwnedonPlaystation;
+var gamesOwnedOnSteam = 61;
+var gamesOwnedOnPlaystation = 51;
+var totalGamesOwned = gamesOwnedOnSteam + gamesOwnedOnPlaystation;
 var isGamer = true;
 
 // String concatenations
@@ -12,16 +12,18 @@ console.log("My name is " + firstName);
 console.log("My favorite games are " + favoriteGame + " and " + otherFavoriteGame);
 
 // Math operations
-console.log("Games owned on Steam after buying 5 more: " + (gamesOwnedonSteam + 5));
-console.log("Games owned on PlayStation after buying 5 more: " + (gamesOwnedonPlaystation + 5));
+console.log("Games owned on Steam after buying 5 more: " + (gamesOwnedOnSteam + 5));
+console.log("Games owned on PlayStation after buying 5 more: " + (gamesOwnedOnPlaystation + 5));
 console.log("My age in 5 years: " + (age + 5));
 
 // Boolean
 console.log("Am I a gamer? " + isGamer);
 
-document.getElementById("content").innerHTML += "<p>I currently own " + gamesOwnedonSteam + " games on Steam and " + gamesOwnedonPlaystation + " games on PlayStation. I enjoy"
+// Finds the HTML element with the id "content" and appends a paragraph to its existing content.
+document.getElementById("content").innerHTML += "<p>I currently own " + gamesOwnedOnSteam + " games on Steam and " + gamesOwnedOnPlaystation + " games on PlayStation. I enjoy"
                 + " playing a variety of games, from action-packed shooters to relaxing"
                 + " simulation games. Each game offers a unique experience and allows me"
                 + " to explore different worlds and stories.</p>";
 
+// Finds the same "content" element and appends another paragraph showing the total number of games.
 document.getElementById("content").innerHTML += "<p>That makes a total of " + totalGamesOwned + " games owned not counting Epic Games.</p>";
